82a343db9ef1305812d248d3b4f77b050abf3861
anceluka <73470979+anceluka@users.noreply.github.com>
2021-09-28 13:59:12 +0300

    darbs

---
+++ pd_ance.js
@@ -0,0 +1,13 @@
+function function1() { 
+    document.body.style.backgroundColor = 'gray'; 
+    } 
+    function function2() { 
+    document.body.style.backgroundColor = 'white';  
+    } 
+    function function3() { 
+    var c = document.getElementById("myCanvas");
+    var ctx = c.getContext("2d");
+    ctx.beginPath();
+    ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+    ctx.stroke();
+    }
